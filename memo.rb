c3ee1a167c425f9fa0396564b0375bb6c5ba565e
mao19991128 <mao19991128@icloud.com>
2023-02-21 20:10:24 +0900

    課題再提出

--- memo.rb
+++ memo.rb
@@ -18,7 +18,10 @@ if memo_type == 1
 
 elsif memo_type == 2
     puts "既存のメモを編集します。拡張子を除いた既存ファイル名を入力してください。"
-    file_name = gets.chomp        
+    file_name = gets.chomp 
+    
+    puts "メモしたい内容を記入してください"
+    puts "完了したらCtrl+Dをおします"
     
     memo = STDIN.read
 
