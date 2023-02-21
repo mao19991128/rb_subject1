fa4501e6df0300586a8901ca40256efc6bc25210
mao19991128 <mao19991128@icloud.com>
2023-02-21 18:42:55 +0900

    課題提出

---
+++ memo.rb
@@ -0,0 +1,32 @@
+require "csv" 
+
+puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
+memo_type = gets.to_i 
+ 
+if memo_type == 1
+    puts "拡張子を除いたファイルを入力してください"
+    file_name = gets.chomp
+    
+    puts "メモしたい内容を記入してください"
+    puts "完了したらCtrl+Dをおします"
+    
+    memo = STDIN.read
+
+    CSV.open("#{file_name}.csv","w") do |csv|
+        csv << ["#{memo}"]
+    end
+
+elsif memo_type == 2
+    puts "既存のメモを編集します。拡張子を除いた既存ファイル名を入力してください。"
+    file_name = gets.chomp        
+    
+    memo = STDIN.read
+
+    CSV.open("#{file_name}.csv","a") do |csv|
+        csv << ["#{memo}"]
+    end
+end
+
+
+
+
